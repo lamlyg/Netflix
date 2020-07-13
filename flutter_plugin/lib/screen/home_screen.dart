01d3a0149c701d43d868f7829ec8bea29b4efd45
lamlyg <lamlyg@naver.com>
2020-07-14 01:28:57 +0900

    home : 캐로셀 슬라이더 위젯

--- flutter_plugin/lib/screen/home_screen.dart
+++ flutter_plugin/lib/screen/home_screen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin/model/model_movie.dart';
+import 'package:flutter_plugin/widget/carousel_slider.dart';
 
 //Movie DB from backend ==> statefulwidget
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사이코지만 괜찮아',
+      'keyword': '가족/로맨스/코미디',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사이코지만 괜찮아',
+      'keyword': '가족/로맨스/코미디',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사이코지만 괜찮아',
+      'keyword': '가족/로맨스/코미디',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사이코지만 괜찮아',
+      'keyword': '가족/로맨스/코미디',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+  ];
   @override
   void initState() {
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context){
-    return TopBar();
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
@@ -26,7 +63,7 @@ class TopBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Image.asset(
-            'images/netflix_logo.png',
+            'images/bbongflix_logo.png',
             fit: BoxFit.contain,
             height: 25,
           ),
